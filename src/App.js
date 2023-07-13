e42133a357105367c6d620953920dca6c3cabd89
Jack <jarbus@tutanota.com>
2023-07-13 10:32:55 -0400

    add: support for new api, prev gen button

--- src/App.js
+++ src/App.js
@@ -2,82 +2,73 @@ import logo from './logo.svg';
 import './App.css';
 import React, { useState } from "react";
 
-// make id a random int
-let id = Math.floor(Math.random() * 1000000000);
+// make genesis genesis_id a random int
+let genesis_id = Math.floor(Math.random() * 1000000000);
+var children = []
 var new_generation = false;
 // global variable of first parent selected for crossover.
 // is reset when a new parent is selected for mutation, or 
 // when a second parent is selected for crossover
 var crossover_parent = 0; 
+var GENERATION = 0
 const genesis_endpoint = 'http://localhost:8000/genesis';
 const submit_endpoint = 'http://localhost:8000/submit_prompt';
-const crossover_endpoint = 'http://localhost:8000/crossover_prompts';
-const inc_gen_endpoint = 'http://localhost:8000/increment_generation/';
 const get_child_endpoint= 'http://localhost:8000/get_new_children';
+const download_endpoint= 'http://localhost:8000/download';
 
-function sendGenesisPrompt(prompt) {
-  const response = fetch(genesis_endpoint, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({prompt: prompt, id: id}),
-  })
-  .then(r => r.json())
-  .then(data => {
-    console.log(data.message);
-    return data.message;
-  })
-  .catch((error) => {
-    console.error('Error:', error);
-  });
+function getPPIDs(children) {
+    var ppids = children.map((child) => child.ppid);
+    var ppid2s = children.map((child) => child.ppid2);
+    // create a set of all ppids
+    var ppids_set = new Set(ppids);
+    var ppid2s_set = new Set(ppid2s);
+    // get the union of the two sets
+    var union = new Set([...ppids_set, ...ppid2s_set]);
+    // remove all null values
+    union.delete(null);
+    union.delete("None");
+    return union;
 }
 
-// constantly pull new children from the server
-async function getNewChildren(child_fetcher, setChildFetcher, cur_gen, setCurGen) {
-  if (child_fetcher !== 0) {
-    return;
-  }
-  setChildFetcher((state) => state+1);
-  var children = []
-
-  while (true) {
+async function updateChildren(props, children){
     // reset children if new generation
+    props.setGenerations(GENERATION);
     if (new_generation) {
       children = [];
       new_generation = false;
     }
     var url = new URL(get_child_endpoint);
-    url.search = new URLSearchParams({ident: id});
+    var pids = children.map((child) => child.pid);
+
+    url.search = new URLSearchParams({genesis_id: genesis_id, gen: GENERATION, seen_pids: pids});
     const response = await fetch(url);
     if (response.status === 200) {
       const data = await response.json()
-      if (data.children.length > 0) {
-        children = children.concat(data.children);
-        setCurGen(children);
+      if (data.length > 0) {
+        children = children.concat(data);
+        props.setCurGen(children);
       }
     }
-    await new Promise(r => setTimeout(r, 1000));
-  }
+    var unique_ppids = getPPIDs(children);
+    props.setNumParents(unique_ppids.size);
+
 }
 
-async function incrementGeneration() {
-  return await fetch(inc_gen_endpoint, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({id: id}),
-    })
-    .catch((error) => {
-      console.error('Error:', error);
-    });
+// constantly pull new children from the server
+async function getNewChildren(props, children) {
+  if (props.child_fetcher !== 0) {
+    return;
+  }
+  props.setChildFetcher((state) => state+1);
+  while (true) {
+    await updateChildren(props, children);
+    await new Promise(r => setTimeout(r, 1000));
+  }
 }
 
 function Square(idx, props) {
   // has attributes prompt image
   var member = props.cur_gen[idx];
-  console.log(idx, props.cur_gen);
   var imgStyle = { maxHeight: "270px", maxWidth: "270px", margin: "0px" };
   var img = "";
   if (member === undefined) {
@@ -87,7 +78,6 @@ function Square(idx, props) {
     img.src =
       "data:image/jpg;base64," + member.image.replace(/^"(.*)"$/, "$1");
 
-    console.log(img.src);
   }
   return (
   <div
@@ -113,10 +103,9 @@ function Square(idx, props) {
           event.target.style.border = "1px solid black";
         } else {
           // send crossover request to server
-          props.setSubmittedThisGen((prev) => prev+1);
-          const data = { p1: crossover_parent, p2: member.prompt, id: id };
+          const data = { prompt: crossover_parent, prompt2: member.prompt, genesis_id: genesis_id, gen: GENERATION };
           crossover_parent = 0;
-          const response = fetch(crossover_endpoint, {
+          const response = fetch(submit_endpoint, {
             method: "POST",
             headers: {
               "Content-Type": "application/json",
@@ -126,9 +115,8 @@ function Square(idx, props) {
         }
       } 
       else {
-        props.setSubmittedThisGen((prev) => prev+1);
         event.target.style.border = "4px solid red";
-        const data = { prompt: member.prompt, id: id };
+        const data = { prompt: member.prompt, gen: GENERATION, genesis_id: genesis_id };
         const response = fetch(submit_endpoint, {
           method: "POST",
           headers: {
@@ -146,29 +134,47 @@ function Square(idx, props) {
 }
 
 function Grid({n, props}) {
-  const squares = Array(n * n).fill(null);
+  const squares = Array(n).fill(null);
 
   return (
-    <div style={{ display: "grid", gridTemplateColumns: `repeat(${6}, 280px)`, gridGap: '10px' }}>
+    <div style={{ display: "grid", gridTemplateColumns: `repeat(${4}, 280px)`, gridGap: '10px' }}>
       {squares.map((square, index) => Square(index, props))}
     </div>
   );
 }
 
-function NextGenerationButton({props, grid}) {
-  const [text, setText] = useState("");
-  // set all image borders to all squares to black
-  
+function PrevGenerationButton({props, grid}) {
   return (
-    <div>
       <button 
         onClick={() => {
-          incrementGeneration(props.setCurGen);
-          console.log('incremented generation');
           new_generation = true;
-          props.setSubmittedThisGen(0);
           props.setCurGen([]);
-          props.setGenerations((prev) => prev+1);
+          GENERATION -= 1;
+          props.setGenerations(GENERATION);
+          updateChildren(props, children);
+          
+          // set all borders of all image elements to black
+          var images = document.getElementsByTagName('img');
+          for (var i = 0; i < images.length; i++) {
+            images[i].style.border = "1px solid black";
+          }
+        }}
+        >
+        Prev Generation
+      </button>
+    );
+}
+
+function NextGenerationButton({props, grid}) {
+  return (
+      <button 
+        onClick={() => {
+          new_generation = true;
+          props.setCurGen([]);
+          GENERATION += 1;
+          props.setGenerations(GENERATION);
+          updateChildren(props, children);
+          
           // set all borders of all image elements to black
           var images = document.getElementsByTagName('img');
           for (var i = 0; i < images.length; i++) {
@@ -178,7 +184,33 @@ function NextGenerationButton({props, grid}) {
         >
         Next Generation
       </button>
-      <p>{text}</p>
+  );
+}
+
+function download(filename, text) {
+  var element = document.createElement('a');
+  element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
+  element.setAttribute('download', filename);
+  element.style.display = 'none';
+  document.body.appendChild(element);
+  element.click();
+  document.body.removeChild(element);
+}
+
+function DownloadButton({props}) {
+  return (
+    <div>
+      <button onClick={() => {
+        var url = new URL(download_endpoint);
+        url.search = new URLSearchParams({ident: genesis_id});
+        fetch(url)
+          .then(r => r.json())
+          .then(json => {
+            var text = JSON.stringify(json);
+            download("generations.json", text);
+          })
+
+      }}>Download</button>
     </div>
   );
 }
@@ -193,14 +225,17 @@ function PromptBox({props}) {
         onChange={(event) => setText(event.target.value)}
       />
     <button onClick={() => {
+        GENERATION = 1;
         props.setGenerations(1);
         props.setCurGen([]);
         props.setPrompt(text);
         new_generation = true;
-        var prompts = sendGenesisPrompt(text);
+        fetch(genesis_endpoint, {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json', },
+            body: JSON.stringify({prompt: text, genesis_id: genesis_id, gen: 0})
+        });
       }}>New Genesis</button>
-    <p>Generation: {props.generations}</p>
-    <p>Prompt: {props.prompt}</p>
     </div>
   );
 }
@@ -210,7 +245,7 @@ function App() {
   const [prompt, setPrompt] = useState("");
   const [cur_gen, setCurGen] = useState([]);
   const [child_fetcher, setChildFetcher] = useState(0);
-  const [submitted_this_gen, setSubmittedThisGen] = useState(0);
+  const [num_parents, setNumParents] = useState(0);
 
   var props = {
     prompt: prompt,
@@ -219,24 +254,29 @@ function App() {
     setGenerations: setGenerations,
     cur_gen: cur_gen,
     setCurGen: setCurGen,
-    submitted_this_gen: submitted_this_gen,
-    setSubmittedThisGen: setSubmittedThisGen,
+    num_parents: num_parents,
+    setNumParents: setNumParents,
+    child_fetcher: child_fetcher,
+    setChildFetcher: setChildFetcher,
   }
-  getNewChildren(child_fetcher, setChildFetcher, cur_gen, setCurGen)
+  getNewChildren(props, children)
 
-  var grid = Grid({n: 4, props: props});
-  if (submitted_this_gen >= 4) {
-    var next_gen_button = <NextGenerationButton props={props} grid={grid}/>;
+  var grid = Grid({n: cur_gen.length, props: props});
+  if (GENERATION > 1) {
+    var prev_gen_button = <PrevGenerationButton props={props} grid={grid}/>;
   } else {
-    var next_gen_button = <div></div>;
+    var prev_gen_button = <div></div>;
   }
+  var next_gen_button = <NextGenerationButton props={props} grid={grid}/>;
 
   return (
     <div className="App">
-    id {id}
+    genesis_id {genesis_id}
     <PromptBox props={props} />
-    Submitted {submitted_this_gen}/4 parents
-    {next_gen_button}
+    {prev_gen_button} Generation: {props.generations} {next_gen_button}
+    <p>Prompt: {props.prompt}</p>
+    <DownloadButton/>
+    This generation has {num_parents} parent(s)
     {grid}
     </div>
   );
